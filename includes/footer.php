7abf90f2ba805faab04fb92dd1f757e493198b63
Ivaylo Trepetanov <ivaylo.i.trepetanov@gmail.com>
2025-01-23 01:57:36 +0200

    added fastcache and fixed facebook issues

--- includes/footer.php
+++ includes/footer.php
@@ -157,20 +157,20 @@
 <script src="/assets/js/jquery-countdown.min.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
 <script src="/assets/js/owl.carousel.min.js"></script>
-<script src="/assets/js/magnific-popup.min.js"></script>
+<script src="/assets/js/magnific-popup.min.js" defer></script>
 <script src="/assets/js/isotope.min.js"></script>
 <script src="/assets/js/jquery.scrollbar.min.js"></script>
 <script src="/assets/js/jquery-ui.min.js"></script>
 <script src="/assets/js/mobile-menu.js"></script>
 <script src="/assets/js/chosen.min.js"></script>
-<script src="/assets/js/slick.js"></script>
+<script src="/assets/js/slick.js" defer></script>
 <script src="/assets/js/jquery.elevateZoom.min.js"></script>
 <script src="/assets/js/jquery.actual.min.js"></script>
 <script src="/assets/js/fancybox/source/jquery.fancybox.js"></script>
-<script src="/assets/js/lightbox.min.js"></script>
+<script src="/assets/js/lightbox.min.js" defer></script>
 <script src="/assets/js/owl.thumbs.min.js"></script>
 <script src="/assets/js/jquery.scrollbar.min.js"></script>
-<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC3nDHy1dARR-Pa_2jjPCjvsOR4bcILYsM'></script>
+<script src="/assets/js/googlemaps.js" defer></script>
 <script src="/assets/js/frontend-plugin.js"></script>
 <script src="/assets/js/app.js"></script>
 </body>
